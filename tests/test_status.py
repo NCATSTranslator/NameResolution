a787e4d8f5bdf31aa0a9d38b51d13ba28f5e1c97
Gaurav Vaidya <gaurav@renci.org>
2026-02-20 18:17:41 -0500

    Added basic tests for /status.

---
+++ tests/test_status.py
@@ -0,0 +1,27 @@
+import logging
+
+from api.server import app
+from fastapi.testclient import TestClient
+
+# Turn on debugging for tests.
+logging.basicConfig(level=logging.DEBUG)
+
+def test_status():
+    client = TestClient(app)
+    response = client.get("/status")
+    status = response.json()
+
+    assert status['status'] == 'ok'
+    assert status['message'] != ''
+    assert 'babel_version' in status
+    assert 'babel_version_url' in status
+    assert 'biolink_model' in status
+    assert 'tag' in status['biolink_model']
+    assert 'nameres_version' in status
+
+    assert status['startTime']
+    assert status['numDocs'] == 32
+    assert status['maxDocs'] == 32
+    assert status['deletedDocs'] == 0
+    assert status['version'].startswith('9.')
+    assert status['size'] != ''
